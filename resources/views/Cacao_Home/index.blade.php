c0a8f13deae9e4b96faa96b8e2789e44e3e03051
EdwinJuanez97 <edinjuanez@unah.hn>
2022-09-27 21:50:56 -0600

    Arreglos 27 Septiembre

---
+++ resources/views/Cacao_Home/index.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Bienvenido a Cacao Express</h1>
+@stop
+
+@section('content')
+    <p>¡El verdadero sabor del cacao!.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+
+
